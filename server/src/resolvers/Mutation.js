5052f632084a371b7ef6197161e9b7b70d1ac5cf
Tess Huang <tesshuangxj@yahoo.com>
2020-09-18 15:05:26 -0700

    Added user connection to post

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -3,9 +3,15 @@ const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
 function post(parent, args, context) {
+  const userId = getUserId(context);
   return context.prisma.createLink({
     url: args.url,
     description: args.description,
+    postedBy: {
+      connect: {
+        id: userId
+      }
+    }
   })
 }
 

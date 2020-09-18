e17fa988ca171f0b84bab6193b05f94631d46901
Tess Huang <tesshuangxj@yahoo.com>
2020-09-18 13:42:28 -0700

    Setup basic login comp and logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import { AUTH_TOKEN } from '../constants';
 
 class Header extends Component {
   render() {
+    const authToken = localStorage.getItem(AUTH_TOKEN);
     return (
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
           <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            all news
-          </Link>
-          <div className="ml1">|</div>
-          <Link to="/create" className="ml1 no-underline black">
-            submit
-          </Link>
+            <Link to="/" className="ml1 no-underline black">
+              all news
+            </Link>
+            {authToken && (
+              <div className="flex">
+                <div className="ml1">|</div>
+                <Link to="/create" className="ml1 no-underline black">
+                  submit
+                </Link>
+              </div>
+            )}
+        </div>
+        <div>
+          {authToken ? (
+            <div className="ml1 pointer black" onClick={() => {localStorage.removeItem(AUTH_TOKEN); this.props.history.push('/')}}>logout</div>
+          ) : (
+            <Link to="/login" className="ml1 no-underline black">
+              login
+            </Link>
+          )}
         </div>
       </div>
     )

574682510ad0107280c084a3325d79ceb21de558
Tess Huang <tesshuangxj@yahoo.com>
2020-09-22 13:28:48 -0700

    Subscribe to create new links

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -25,14 +25,65 @@ export const FEED_QUERY = gql`
     }
   }
 `
-
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id 
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
 class LinkList extends Component {
+  _updateCacheAfterVote = (store, createVote, linkId) => {
+    const data = store.readQuery({ query: FEED_QUERY });
+
+    const votedLink = data.feed.links.find(link => link.id === linkId);
+    votedLink.votes = createVote.link.votes;
+
+    store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  _subscribeToNewLink = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if(!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if(exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename,
+          }
+        })
+      }
+    })
+  }
+
   render() {
     return (
      <Query query={FEED_QUERY}>
-       {({loading, error, data}) => {
+       {({loading, error, data, subscribeToMore }) => {
          if(loading) return <div>Loading...</div>
          if(error) return <div>Error happened</div>
+
+         this._subscribeToNewLink(subscribeToMore);
+
          const linksToRender = data.feed.links;
          return (
            <div>
@@ -42,14 +93,6 @@ class LinkList extends Component {
          }}
      </Query>
     )
-  }
-  _updateCacheAfterVote = (store, createVote, linkId) => {
-    const data = store.readQuery({ query: FEED_QUERY });
-
-    const votedLink = data.feed.links.find(link => link.id === linkId);
-    votedLink.votes = createVote.link.votes;
-
-    store.writeQuery({ query: FEED_QUERY, data })
   }
 }
 
